2e432f0765d26bfebaa3a2a0cb6e51331dfddd19
meirislene <meirislene@hotmail.com>
2023-07-07 14:11:12 -0300

    site loja

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-let carouselltems = document.querySelectorAll('.carrossel');
-let currentlndex =0;
+let carouselItems = document.querySelectorAll('.carrossel li');
+let currentlndex = 0;
 
 function showSlide(index){
     //Remove a classe "active"de todos os itens do carrossel 
-     carouselltems,forEach(function(item){
-        item.classlist.remove('actve');
+     carouselItems.forEach(function(item){
+        item.classlist.remove('active');
      });
      //adiciona a classe "active" aoitem atual
-     carouselltems[index.classlist.add('active');]
+     carouselItems[index].classList.add('active');
 }
+
+function nextSlide(){
+   currentIndex++;
+   if(currentIndex >= carouselItems.length){
+      currentIndex = 0;
+   }
+   showSlide(currentIndex);
+
+}
+
+function prevSlide(){
+   currentIndex--;
+   if(currentIndex < 0){
+      currentIndex = carouselItems.length-1;
+   }
+   showSlide(currentIndex);
+   }
+
+   document.querySelector('.carousel-control-prev').addEventListener('click,prevSlide');
+   document.querySelector('.carousel-control-next').addEventListener('click,nextSlide');
+
+   //exibe o primeiro slide ao carregara pagina
+   showSlide(currentIndex);
